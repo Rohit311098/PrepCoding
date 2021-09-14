1e93bee06d01d07b1c039083ae32de0139573422
Rohit <90521597+Rohit311098@users.noreply.github.com>
2021-09-14 23:32:34 +0530

    Create Print Maze Paths With Jumps.java

---
+++ Recursion/Recursion on the way up/Print Maze Paths With Jumps.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn=new Scanner(System.in);
+		int n=scn.nextInt();
+		int m=scn.nextInt();
+		printMazePaths(0,0,n-1,m-1,"");
+
+    }
+
+    // sr - source row
+    // sc - source column
+    // dr - destination row
+    // dc - destination column
+    public static void printMazePaths(int sr, int sc, int dr, int dc, String ans) {
+        if(sr==dr && sc==dc)
+    	{
+    		System.out.println(ans);
+    		return;
+    	}
+    	
+    
+    	for(int i=1;sc+i<=dc;i++)
+    	{
+    		printMazePaths(sr,sc+i,dr,dc,ans+"h"+i);
+	    	
+    	}
+    	
+    	
+    	for(int i=1;sr+i<=dr;i++)
+    	{
+    		printMazePaths(sr+i,sc,dr,dc,ans+"v"+i);
+	    	
+    	}
+    	
+    	
+    	for(int i=1;sr+i<=dr && sc+i<=dc;i++)	
+    	{
+    		printMazePaths(sr+i,sc+i,dr,dc,ans+"d"+i);
+	    	
+    	}
+        
+    }
+
+}
