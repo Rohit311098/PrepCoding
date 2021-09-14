20733b60580a9813893ae7b5834f9fe9a34e5614
Rohit <90521597+Rohit311098@users.noreply.github.com>
2021-09-14 23:25:02 +0530

    Create Get Subsequence.java

---
+++ Recursion/Recursion in ArrayList/Get Subsequence.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc=new Scanner(System.in);
+		String str=sc.next();
+		ArrayList<String> ans=gss(str);
+		System.out.println(ans);
+
+    }
+
+    public static ArrayList<String> gss(String str) {
+        if(str.length()==0)
+		{
+			ArrayList<String> ans=new ArrayList<String>();
+			ans.add("");
+			return ans;
+		}
+		ArrayList<String> temp=gss(str.substring(1));
+		ArrayList<String> result=new ArrayList<String>();
+		for(String s: temp)
+		{
+			result.add(s);
+		}
+		for(String s:temp)
+		{
+			result.add(str.charAt(0)+s);
+		}
+        return result;
+    }
+
+}
