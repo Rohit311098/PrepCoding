657076e720122d5ccc0dac50282be10554ea2440
Rohit <90521597+Rohit311098@users.noreply.github.com>
2021-09-24 23:18:24 +0530

    Create Next Greater Element To The Right.java

---
+++ Stack and Queue/Next Greater Element To The Right.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+  public static void display(int[] a){
+    StringBuilder sb = new StringBuilder();
+
+    for(int val: a){
+      sb.append(val + "
+");
+    }
+    System.out.println(sb);
+  }
+
+public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    int[] a = new int[n];
+    for(int i = 0; i < n; i++){
+       a[i] = Integer.parseInt(br.readLine());
+    }
+
+    int[] nge = solve(a);
+    display(nge);
+ }
+
+ public static int[] solve(int[] arr){
+   // solve
+   int[] ans=new int[arr.length];
+   Stack<Integer> stk=new Stack<Integer>();
+   
+   for(int i=arr.length-1;i>=0;i--)
+   {
+       
+       while(stk.isEmpty()!=true && stk.peek()<arr[i])
+       {
+          stk.pop();   
+       }
+       if(stk.isEmpty()==true)
+       {
+           ans[i]=-1;
+       }
+       else
+       {
+           ans[i]=stk.peek();
+       }
+       stk.push(arr[i]);
+   }
+   return ans;
+ }
+
+}
