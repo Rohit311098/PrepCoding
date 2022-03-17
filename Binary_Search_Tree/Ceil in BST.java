ae070deda02c4d4aae17f708e4f69389efcce0c5
Rohit <90521597+Rohit311098@users.noreply.github.com>
2022-03-17 21:27:33 +0530

    Update Ceil in BST.java

--- Binary_Search_Tree/Ceil in BST.java
+++ Binary_Search_Tree/Ceil in BST.java
@@ -1,38 +1,30 @@
 class Tree {
     // Function to return the ceil of given number in BST.
     
-    int ceil;
-    
-    void helper(Node root,int key)
-    {
-        if(root==null)
-        {
-            return;
-        }
-        
-        if(root.data==key)
-        {
-            ceil=root.data;
-            return;
-        }
-        
-        if(root.data>key)
-        {
-            ceil=root.data;
-            helper(root.left,key);
-        }
-        else
-        {
-            helper(root.right,key);
-        }
-        
-        
-    }
+
     int findCeil(Node root, int key) {
         if (root == null) return -1;
         
-        ceil=-1;
-        helper(root,key);
+        int ceil=-1;
+        
+        while(root!=null)
+        {
+            
+            if(root.data==key)
+            {
+                return root.data;
+            }
+            
+            if(root.data>key)
+            {
+                ceil=root.data;
+                root=root.left;
+            }
+            else
+            {
+                root=root.right;
+            }
+        }
         return ceil;
     }
 }
